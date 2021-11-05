a9602164b5275eada6f1d0586694a8cc2b200f3a
plerros <48536508+plerros@users.noreply.github.com>
2021-11-05 17:29:33 +0200

    fput_any_word takes udword

--- src/file_utils/file_utils.c
+++ src/file_utils/file_utils.c
@@ -28,12 +28,12 @@ udword_t dword_abs(dword_t value)
 	return value;
 }
 
-static int fput_any_word(unsigned long long *value, size_t size, FILE *fp)
+static int fput_any_word(udword_t value, size_t size, FILE *fp)
 {
 	int rc = 0;
 	for (size_t i = 0; i < size && !rc; i++) {
-		int tmp = *value % (UCHAR_MAX + 1); // & 0x0f would probably be just fine
-		*value = *value >> CHAR_BIT;
+		int tmp = value % (UCHAR_MAX + 1); // & 0x0f would probably be just fine
+		value >>= CHAR_BIT;
 		rc = (fputc(tmp, fp) != tmp);
 	}
 	return rc;
@@ -41,19 +41,16 @@ static int fput_any_word(unsigned long long *value, size_t size, FILE *fp)
 
 int fput_uword(uword_t value, FILE *fp)
 {
-	unsigned long long pval = value;
-	return (fput_any_word(&pval, 2, fp));
+	return (fput_any_word(value, 2, fp));
 }
 
 int fput_dword(dword_t value, FILE *fp)
 {
-	udword_t u_value = value;
-	unsigned long long pval = u_value;
-	return (fput_any_word(&pval, 4, fp));
+	udword_t *u_value = (void *)&value;
+	return (fput_any_word(*u_value, 4, fp));
 }
 
 int fput_udword(udword_t value, FILE *fp)
 {
-	unsigned long long pval = value;
-	return (fput_any_word(&pval, 4, fp));
+	return (fput_any_word(value, 4, fp));
 }
